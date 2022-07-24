da39aceaef3770ef0aa7de73373cf18b2e08be78
Rory Kaufmann <rorykaufmann@gmail.com>
2022-07-23 22:57:58 -0500

    added plot library, getting ready for heroku deploy

--- app/routes/api/orderHistory.js
+++ app/routes/api/orderHistory.js
@@ -23,7 +23,6 @@ function getOrderHistory(req, res) {
 }
 
 function appendToOrderHistory(req, res) {
-    console.log(req.body)
     let order = req.body;
     let itemHrid = order.itemHrid;
     let time = Date.now()
@@ -33,39 +32,15 @@ function appendToOrderHistory(req, res) {
          { upsert: true}
          )
         .then(result => res.send(result))
-        .catch(err => {
-            console.log(err);
-            res.status(500).send(err);
-         });
+        .catch(err => res.status(500).send(err));
 
 }
 
-//format is
+//format for order update is
 //{"type":"market_item_order_books_updated","marketItemOrderBooks":{"itemHrid":"/items/donut","orderBooks":[{"asks":[{"listingId":192902,"quantity":252,"price":36},{"listingId":192828,"quantity":2344,"price":37},{"listingId":191681,"quantity":109,"price":44},{"listingId":191261,"quantity":9060,"price":47},{"listingId":179376,"quantity":30,"price":190},{"listingId":177749,"quantity":9,"price":245},{"listingId":175887,"quantity":235,"price":250},{"listingId":149309,"quantity":950,"price":500},{"listingId":26516,"quantity":8,"price":5000},{"listingId":33417,"quantity":1,"price":1000000000000}],"bids":[{"listingId":185915,"quantity":3630,"price":8},{"listingId":179404,"quantity":500,"price":7},{"listingId":83884,"quantity":2996,"price":6}]}]}}
 //
 // unwrapped on client, and hrid changed to format "items-name_of_item"
-function parseOrderBook(orderBooks) {
-    orderBooks.time = Date.now()
-    //validate orderBooks here
-    validateItemHrid(orderBooks);
-    
-    return orderBooks;
-}
 
-function validateItemHrid(itemHrid) {
-    let path = itemHrid.split("-");
-
-    let type = path[0]
-    let name = path[1]
-    if(type == 'items') {
-
-    } else if(type == 'abilities') {
-
-    } else {
-        return false;
-    }
-    return true;
-}
 
 //get thumbnails from itemhrid
 //hrid is format <folder>-name_of_item
