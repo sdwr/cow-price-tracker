645e3596232b263adb5d576960ad77f21eaca81c
Rory Kaufmann <rorykaufmann@gmail.com>
2022-07-23 23:23:02 -0500

    didnt need full URL in api calls :|

--- app/web/api.js
+++ app/web/api.js
@@ -1,11 +1,9 @@
-const SERVER_URL = 'http://localhost:8080/api'
-
 function getAllItems() {
     const options = {}
     options.method = 'GET';
     options.mode = 'cors';
     options.headers = {'Content-Type': 'application/json'}
-    return fetch(SERVER_URL + "/items", options)
+    return fetch("api/items", options)
 }
 
 function getOrderHistory(id) {
@@ -13,7 +11,7 @@ function getOrderHistory(id) {
     options.method = 'GET';
     options.mode = 'cors';
     options.headers = {'Content-Type': 'application/json'}
-    return fetch(SERVER_URL + `/orderHistory/${id}`, options)
+    return fetch(`api/orderHistory/${id}`, options)
 }
 
 function appendToOrderHistory(orderBooks) {
@@ -22,7 +20,7 @@ function appendToOrderHistory(orderBooks) {
     options.mode = 'cors';
     options.headers = {'Content-Type': 'application/json'}
     options.body = JSON.stringify(orderBooks)
-    return fetch(SERVER_URL + `/orderHistory/${orderBooks.itemHrid}`, options)
+    return fetch(`api/orderHistory/${orderBooks.itemHrid}`, options)
 }
 
 export const api = {getAllItems, getOrderHistory, appendToOrderHistory}
