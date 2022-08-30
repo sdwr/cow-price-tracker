c6fb070a41926bd2e7ba267839598336494a5ae1
Rory Kaufmann <rorykaufmann@gmail.com>
2022-08-30 02:47:57 -0500

    test for linode

--- app/mongo/db.js
+++ app/mongo/db.js
@@ -5,6 +5,7 @@ const dbName = 'cow-price-tracker';
 mongoose.connect(url, {useNewUrlParser: true});
 db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error:'));
-db.once('open', () => console.log('mongo connected'));
+db.once('open', () => console.log('mongo connected. collections:'));
+db.once('open', () => console.log(Object.keys(db.collections)));
 
 module.exports = db;
